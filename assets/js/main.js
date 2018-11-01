b53bd748e7bf005226821013f1987a1b7bac7303
Gavin Barnett <gavbarnett@gmail.com>
2018-11-01 17:06:45 +0000

    fix distance

--- assets/js/main.js
+++ assets/js/main.js
@@ -367,13 +367,13 @@ function field(){
                     itemSelected = i
                 }
             }
-            if (minDist<(2*scaler)){
+            if (minDist<(2)){
                 Field.PlaceEquipment()
                 tempEquipment = PlacedEquipment[itemSelected]
                 PlacedEquipment.splice(itemSelected, 1)
             } else {
                 distance = Math.sqrt(Math.pow((tempEquipment.x-mouse.x),2)+Math.pow((tempEquipment.y-mouse.y),2))
-                if(distance<(2*scaler)){
+                if(distance<(2)){
                     //keep focus on current item and don't place it yet.
                 } else {
                     Field.PlaceEquipment()
@@ -409,13 +409,13 @@ function field(){
                 itemSelected = i
             }
         }
-        if (minDist<(2*scaler)){
+        if (minDist<(2)){
             Field.PlaceEquipment()
             tempEquipment = PlacedEquipment[itemSelected]
             PlacedEquipment.splice(itemSelected, 1)
         } else {
             distance = Math.sqrt(Math.pow((tempEquipment.x-mouse.x),2)+Math.pow((tempEquipment.y-mouse.y),2))
-            if(distance<(2*scaler)){
+            if(distance<(2)){
                 //keep focus on current item and don't place it yet.
             } else {
                 Field.PlaceEquipment()
