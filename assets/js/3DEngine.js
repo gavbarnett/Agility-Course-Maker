48ad6a6b3e86f4ef4276e94fb5f9a4af0908923c
Gavin Barnett <gavbarnett@gmail.com>
2018-11-01 21:48:27 +0000

    one 3d jump

--- assets/js/3DEngine.js
+++ assets/js/3DEngine.js
@@ -1,39 +1,56 @@
+var container;
+var camera, scene, renderer, elf;
+function drawEquipment3D() {
+    container = document.getElementById( 'MainCanvas2' );
+    container.id = "MainCanvas2";
+    container.width = Math.min(window.innerWidth*0.95,window.innerHeight*0.75) 
+    container.height =  container.width
+    container.style.border = "1px solid";
+    container.tabIndex = 1
+    container.focus()
 
-function drawEquipment3D(item, scaler, offset, canvas){
-    console.log("Going 3D")
-    var canvasDiv = document.getElementById('CanvasDiv')
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 2000 );
+    camera.position.set( 8, 10, 8 );
+    camera.lookAt( 0, 3, 0 );
+    scene = new THREE.Scene();
+    // loading manager
+    var loadingManager = new THREE.LoadingManager( function () {
+        scene.add( elf );
+    } );
+    // collada
+    var loader = new THREE.ColladaLoader( loadingManager );
+    loader.load( './assets/3Dassets/Collada Objects/Over-1.dae', function ( collada ) {
+        elf = collada.scene;
+    } );
+    //
+    var ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
+    scene.add( ambientLight );
+    var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
+    directionalLight.position.set( 1, 1, 0 ).normalize();
+    scene.add( directionalLight );
+    //
+    renderer = new THREE.WebGLRenderer( { canvas: MainCanvas2 } );
+				//renderer = new THREE.WebGLRenderer();
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( document.getElementById( 'MainCanvas2' ).width, document.getElementById( 'MainCanvas2' ).height );
+    //container.appendChild( renderer.domElement );
+    //
+    window.addEventListener( 'resize', onWindowResize, false );
+    animate();
 
-    var renderer = new THREE.WebGLRenderer()
-    canvasDiv.appendChild(renderer.domElement)
-    renderer.setClearColor(0x00ff00)
-    renderer.setPixelRatio(window.devicePixelRatio)
-    renderer.setSize(window.innerWidth*0.5, window.innerHeight*0.5)
-
-    var camera  = new THREE.PerspectiveCamera(35,window.innerWidth/window.innerHeight, 0.1, 3000)
-
-    var scene = new THREE.Scene()
-
-    var light = new THREE.AmbientLight(0xffffff, 0.5)
-    scene.add(light)
-
-    var light1 = new THREE.PointLight(0xffffff, 0.5)
-    scene.add(light1)
-
-    var geometry = new THREE.CubeGeometry(100,100,100)
-    var material = new THREE.MeshLambertMaterial({color: 0xF3FFE2})
-    var mesh = new THREE.Mesh(geometry,material)
-    mesh.position.set(0,0,-1000)
-
-    scene.add(mesh)
-
-    var render = function() {
-        mesh.rotation.x +=0.01
-        mesh.rotation.y += 0.1
-        renderer.render(scene, camera)
-        requestAnimationFrame(render)
+}
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+function animate() {
+    requestAnimationFrame( animate );
+    render();
+}
+function render() {
+    if ( elf !== undefined ) {
+    } else {
     }
-
-    requestAnimationFrame(render)
-
-
+    renderer.render( scene, camera );
 }
