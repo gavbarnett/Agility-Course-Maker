a8dc3dc4323f906d5075c40ebd993c148daa7a7c
Gavin Barnett <gavbarnett@gmail.com>
2018-11-06 21:17:31 +0000

    Camera Position at Start

--- assets/js/3DEngine.js
+++ assets/js/3DEngine.js
@@ -14,8 +14,8 @@ function start3D() {
     container.tabIndex = 1
     container.focus()
     camera = new THREE.PerspectiveCamera( 45, container.width / container.height, 0.1, 2000 );
-    camera.position.set( 8, 10, 8 );
-    camera.lookAt( 0, 3, 0 );
+    //camera.position.set( 30, 30, 10 );
+    //camera.lookAt( 15, 3, 15 );
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0xcce0ff );
     scene.fog = new THREE.Fog( 0xcce0ff, 30, 150 );
@@ -25,7 +25,7 @@ function start3D() {
     } );
 
     // collada
-    drawEquipment3D()
+    FieldDraw3D()
 
     //scene
     scene.add( new THREE.AmbientLight( 0x666666 ) );
@@ -43,9 +43,7 @@ function start3D() {
 				light.shadow.camera.far = 1000;
 				scene.add( light );
 
-    camera = new THREE.PerspectiveCamera( 30, container.width / container.height, 1, 10000 );
-    camera.position.set( 20, 5, 20 );
-    camera.lookAt( scene.position )
+    camera = new THREE.PerspectiveCamera( 30, container.width / container.height, 1, 1000 );
 
     // ground
     var loaderT = new THREE.TextureLoader();
@@ -71,27 +69,100 @@ function start3D() {
     var controls = new THREE.OrbitControls( camera, container );
                 controls.maxPolarAngle = Math.PI * 0.49;
 				controls.minDistance = 1;
-				controls.maxDistance = 50;
-   
+                controls.maxDistance = 50;
+                controls.object.position.set(15, 50, 30);
+                controls.target = new THREE.Vector3(15,0,15)
                 //window.addEventListener( 'resize', onWindowResize, false );
-    animate();
+                controls.update();
+                animate();
     
 
 }
 
-function drawEquipment3D(){
-    load( './assets/3Dassets/Collada Objects/Over-1.dae', 0,0,0,0)
-    load( './assets/3Dassets/Collada Objects/Over-1.dae', 5,0,5, Math.PI/2)
-    load( './assets/3Dassets/Collada Objects/Over-3.dae', 3,0,0,0)
-    load( './assets/3Dassets/Collada Objects/Contact-1.dae', 5,0,0,0)
-    load( './assets/3Dassets/Collada Objects/Contact-2.dae', 7,0,0,0)
-    load( './assets/3Dassets/Collada Objects/Contact-3.dae', 8,0,0,0)
+function FieldDraw3D(){
+    for (var i = 0; i <PlacedEquipment.length; i++){
+        drawEquipment3D(PlacedEquipment[i])
+    }
+}
 
-   // load( './assets/3Dassets/Collada Objects/Over-1.dae', 0,0,2,0)
-    //var loader = new THREE.ColladaLoader( loadingManager );
-    //loader.load( './assets/3Dassets/Collada Objects/Over-1.dae', function ( collada ) {
-    //    equipment = collada.scene;
-    //} );
+function drawEquipment3D(item){
+
+    this.drawContact = function(){
+        switch(item.type[1]){
+            case 0: //"A-frame"
+            load( './assets/3Dassets/Collada Objects/Contact-1.dae', item.x, 0,item.y, -item.rotation-Math.PI/2)
+                break
+            case 1: //"Dog Walk"
+            load( './assets/3Dassets/Collada Objects/Contact-2.dae', item.x, 0,item.y, -item.rotation-Math.PI/2)
+                break
+            case 2: //"See Saw"
+                load( './assets/3Dassets/Collada Objects/Contact-3.dae', item.x, 0,item.y, -item.rotation-Math.PI/2)
+                break
+            case 3: //"Table"
+                
+                break
+            case 4: //"Pause Box"
+                
+                break
+        }
+    }
+    this.drawOver = function(){
+        switch(item.type[1]){
+            case 0: //"Winged Single Jump":
+                load( './assets/3Dassets/Collada Objects/Over-1.dae', item.x, 0,item.y, -item.rotation)
+                break
+            case 1: //"Spread":
+                
+                break
+            case 2: //"Tire Jump":
+                load( './assets/3Dassets/Collada Objects/Over-3.dae', item.x, 0,item.y, -item.rotation)
+                break
+        }
+    }
+    this.drawThrough = function(){
+        switch(item.type[1]){
+            case 0: //"Tunnel":
+                break
+            case 1: //"Chute":
+                break
+        }
+    }
+    this.drawWeave = function(){
+        switch(item.type[1]){
+            case 0: //"6":
+                load( './assets/3Dassets/Collada Objects/Pole-1.dae', item.x, 0,item.y, -item.rotation+Math.PI/2)
+                break
+            case 1: //"9":
+                load( './assets/3Dassets/Collada Objects/Pole-2.dae', item.x, 0,item.y, -item.rotation+Math.PI/2)
+                break
+            case 2: //"12":
+                load( './assets/3Dassets/Collada Objects/Pole-3.dae', item.x, 0,item.y, -item.rotation+Math.PI/2)
+                break
+        }
+    }
+    this.drawNumbers = function(){
+        //ctx.fillText((allEquipment["Numbers"][item.type[1]]),0,scaler*0.25);
+    }
+    if (!canvas){
+        canvas = document.getElementById('MainCanvas');
+    }
+    switch(item.type[0]){
+        case "Contact":
+            this.drawContact()
+            break
+        case "Over":
+            this.drawOver()
+            break
+        case "Through":
+            this.drawThrough()
+            break
+        case "Weave":
+            this.drawWeave()
+            break
+        case "Numbers":
+            this.drawNumbers()
+            break
+    }
 }
 
 function load(daeLocation, x, y, z, rot){
@@ -125,8 +196,5 @@ function animate() {
     }
 }
 function render() {
-    if ( equipment !== undefined ) {
-    } else {
-    }
     renderer.render( scene, camera );
 }
