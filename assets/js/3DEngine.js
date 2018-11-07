6847540b4713e7380f89c7733af181ce2b5d34eb
Gavin Barnett <gavbarnett@gmail.com>
2018-11-07 18:00:56 +0000

    updates lightings

--- assets/js/3DEngine.js
+++ assets/js/3DEngine.js
@@ -29,17 +29,17 @@ function start3D() {
 
     //scene
    // LIGHTS
-   hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+   hemiLight = new THREE.HemisphereLight( 0xffcc99, 0xffcc99, 0.6 );
    hemiLight.color.setHSL( 0.6, 1, 0.6 );
    hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
-   hemiLight.position.set( 0, 50, 0 );
+   hemiLight.position.set( 0, 1000, 0 );
    scene.add( hemiLight );
    hemiLightHelper = new THREE.HemisphereLightHelper( hemiLight, 10 );
    scene.add( hemiLightHelper );
    //
-   dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+   dirLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
    dirLight.color.setHSL( 0.1, 1, 0.95 );
-   dirLight.position.set( 60, 50, 60 );
+   dirLight.position.set( 200, 200, 200 );
    dirLight.position.multiplyScalar( 1 );
    scene.add( dirLight );
    dirLight.castShadow = true;
@@ -53,7 +53,7 @@ function start3D() {
    dirLight.shadow.camera.far = 3500;
    dirLight.shadow.bias = - 0.0001;
    dirLightHeper = new THREE.DirectionalLightHelper( dirLight, 10 );
-   //scene.add( dirLightHeper );
+   scene.add( dirLightHeper );
 
 
     camera = new THREE.PerspectiveCamera( 30, container.width / container.height, 1, 1000 );
