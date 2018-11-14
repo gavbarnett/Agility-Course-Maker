ea87f6f4d1d51a4fb3301a6bf161c8f9fdd67c5d
Gavin Barnett <gavbarnett@gmail.com>
2018-11-14 19:24:42 +0000

    removing space below field

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ function main() {
     canvas.id = "MainCanvas";
     canvas.width = Math.min(window.innerWidth*0.95,window.innerHeight*0.75) 
     oldwindowwidth = window.innerWidth
-    canvas.height =  canvas.width * 1.1
+    canvas.height =  canvas.width
     canvas.style.border = "1px solid";
     canvas.tabIndex = 1
     canvas.focus()
@@ -452,7 +452,7 @@ function field(x,y){
         if (window.innerWidth != oldwindowwidth) {
             var canvas = document.getElementById('MainCanvas');
             canvas.width = Math.min(window.innerWidth*0.95,window.innerHeight*0.75) 
-            canvas.height =  canvas.width * 1.1
+            canvas.height =  canvas.width
             this.scaled = [
                 xoffset,
                 yoffset,
